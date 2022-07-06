eb305ccea6db4abfdca53332ce3d3c23636d0ca5
Shahrom <shahromkr@code.edu.az>
2022-07-06 12:43:38 +0400

    Added homework

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,12 @@
-function Click(){
+function Show(){
     var money = Number(document.getElementById("money").value)
     var month = Number(document.getElementById("month").value)
     var percent = Number(document.getElementById("percent").value)
 
-    var mothlyPercent=percent/12*month
-    var totalprice=amount*mothlyPercent/100+money
+    console.log(percent)
+
+    var monthlyPercent=percent/12*month
+    var totalprice=money*monthlyPercent/100+money
 
 
     document.getElementById('TotalAmount').innerHTML = totalprice.toFixed(2) + 'Azn'
